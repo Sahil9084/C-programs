47b969d5bdf945e5eb89d6a348b39a8748082821
Sahil Khan <154963571+Sahil9084@users.noreply.github.com>
2023-12-27 20:48:22 +0530

    Add files via upload

---
+++ MultiplyOfTwoN.c
@@ -0,0 +1,15 @@
+// Multiply of two Numbers c Language
+#include <stdio.h>
+
+int main()
+{
+    float x,y,z;
+    printf("\nEnter The First Number = ");
+    scanf("%f",& x);
+    printf("\nEnter The Secound Number = ");
+    scanf("%f",& y);
+    printf("\nYour Ans = ");
+    z = x*y;
+    printf("%f", z);
+return 0 ;
+}
