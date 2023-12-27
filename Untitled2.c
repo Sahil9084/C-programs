fff6238d03918e8924bbbec6ee3e50db02d6af1c
Sahil Khan <154963571+Sahil9084@users.noreply.github.com>
2023-12-27 20:05:29 +0530

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int ch = 'a';
+	printf("%d\n", &a);
+	return 0 ;
+}
+
---
+++ subtractionOfTwoN.c
@@ -0,0 +1,15 @@
+// Subtraction of two numbers C Language
+#include <stdio.h>
+
+int main()
+{
+    float x,y,z;
+    printf("\nEnter The First Number = ");
+    scanf("%f",& x);
+    printf("\nEnter The Secound Number = ");
+    scanf("%f",& y);
+    printf("\nYour Ans = ");
+    z = x-y;
+    printf("%f", z);
+    return 0 ;
+}
