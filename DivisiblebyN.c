d391f595f8fc84eab11be496a73a62edb880b2bc
Sahil Khan <154963571+Sahil9084@users.noreply.github.com>
2023-12-28 16:27:10 +0530

    Add files via upload

---
+++ DivisiblebyN.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int x ;
+    printf("enter the number ! ");
+    scanf("%d",&x);
+    
+    if(x%5==0)
+    {
+       printf("This number is divisible by 5 :");
+    }
+    else
+    {
+        printf("This number is not divisible by 5 :");
+      
+    }
+
+
+    return 0;
+};
---
+++ graterthanLessThan.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int x ,y ;
+    printf("enter the number ! ");
+    scanf("%d",&x,&y);
+    
+    if(x>y)
+    {
+        
+       printf("x is greater than");
+    }
+    else
+    {
+        printf("y is greater than");
+      
+    }
+
+
+    return 0;
+};
